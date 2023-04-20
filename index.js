9e89acc0d7f2f8e45c6ff0627fd2af7609428db7
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-20 12:01:55 +0200

    function: like and retweet btns

--- index.js
+++ index.js
@@ -8,20 +8,78 @@ const tweetBtn = document.getElementById("tweet-btn")
 document.addEventListener("click", function(e){
 
     if(e.target.dataset.reply){
-        console.log("You clicked reply btn with uuid: ", e.target.dataset.reply)
+        handleReplyClick(e.target.dataset.reply)
     }
     else if(e.target.dataset.like){
-        console.log("You clicked like btn with uuid: ", e.target.dataset.like)
+        handleLikeClick(e.target.dataset.like)
+        
     }
     else if(e.target.dataset.retweet){
-        console.log("You clicked retweet btn with uuid: ", e.target.dataset.retweet)
+        handleRetweetClick(e.target.dataset.retweet)
     }
     else if(e.target.id === "tweet-btn"){
-        console.log("You clicked Tweet btn with id: ", e.target.id)
+        handleTweetClick(e.target.id)
     }
-    
+
 })
 
+function handleReplyClick(tweetId){
+    // Check if the button is working 
+    console.log("You clicked reply btn with uuid: ", tweetId)
+}
+
+function handleLikeClick(tweetId){
+    // Check if the button is working 
+    console.log("You clicked like btn with uuid: ", tweetId)
+
+    // Get tweet object with correct uuid
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0] // Making it an object instead of array
+
+    // Check if object exists
+    console.log(targetTweetObj)
+
+    // Increment likes if not already liked otherwise decrement likes
+    if(targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }else{
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+
+    // Render after updates
+    render() 
+}
+
+function handleRetweetClick(tweetId){
+    // Check if the button is working 
+    console.log("You clicked retweet btn with uuid: ", tweetId)
+
+    // Get tweet object with correct uuid
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0] // Making it an object instead of array
+
+    // Check if object exists
+    console.log(targetTweetObj)
+
+    // Increment likes if not already liked otherwise decrement likes
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+
+    // Render after updates
+    render() 
+}
+
+function handleTweetClick(tweetId){
+    // Check if the button is working 
+    console.log("You clicked Tweet btn with id: ", tweetId)
+}
 
 
 function getFeedHtml(){
@@ -29,6 +87,7 @@ function getFeedHtml(){
     let feedHtml = ""
     
     tweetsData.forEach(function(tweet){
+        
         feedHtml += 
         `
             <div class="tweet">
